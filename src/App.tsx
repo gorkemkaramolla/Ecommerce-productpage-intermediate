7ab21aecd735cc8a5a9ee5eb827960e726bda7a3
gorkemkaramolla <gorkem_karamolla@hotmail.com>
2023-02-12 22:19:30 +0300

    attribution

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,15 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
-
+import React from "react";
+import "./App.css";
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
+      <div className="attribution">
+        Challenge by&nbsp;
+        <a href="https://www.frontendmentor.io?ref=challenge" target="_blank">
+          Frontend Mentor
         </a>
-      </header>
+        . Coded by <a href="#">&nbsp;Görkem Karamolla</a>.
+      </div>
     </div>
   );
 }
